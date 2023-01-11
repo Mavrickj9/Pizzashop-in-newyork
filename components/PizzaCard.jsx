b4e61c2c26dbc618b07c9e75e1e76ec04962b43c
Tinotenda Mupasiri <mavrickj9@gmail.com>
2023-01-11 15:47:02 +0200

    404 error

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -6,7 +6,9 @@ const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
       <Link href={`/product/${pizza._id}`} passHref>
-        <Image src={pizza.img} alt="" width="500" height="500" />
+        <a>
+          <Image src={pizza.img} alt="" width="500" height="500" />
+        </a>
       </Link>
       <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>${pizza.prices[0]}</span>
